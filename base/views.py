440ec7d5b576bbfd8cc4aa87bfee00c60e14a3d1
cadaix <bayarvet@gmail.com>
2022-10-27 12:53:55 +0300

    its done

--- base/views.py
+++ base/views.py
@@ -6,11 +6,11 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from django.contrib.auth.models import User
+
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Message, Room, Topic
-from .forms import RoomForm, UserForm
+
+from .models import Message, Room, Topic, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 
 # rooms = [
@@ -66,10 +66,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -203,7 +203,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST,request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
